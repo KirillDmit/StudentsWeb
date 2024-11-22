1cb8205491b9271fff2252759f81fef42074a7fa
Kirill Dmitriev <kirilldmitriev20015@gmail.com>
2024-11-23 01:26:43 +0500

    add student class

---
+++ src/main/java/ru/doczilla/entity/Student.java
@@ -0,0 +1,71 @@
+package ru.doczilla.entity;
+
+import java.time.LocalDate;
+
+public class Student {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String patronymic;
+    private LocalDate birthDate;
+    private String groupName;
+
+    public Student() {}
+
+    public Student(Long id, String firstName, String lastName, String patronymic, LocalDate birthDate, String groupName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.patronymic = patronymic;
+        this.birthDate = birthDate;
+        this.groupName = groupName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+}
